04d1d64ff8b2ff71df640c19ef9d62917f66c547
SrAllen14 <alvaro.allper.1@educa.jcyl.es>
2026-01-27 12:06:53 +0100

    Configurar proyecto SpringBoot

--- src/main/java/com/example/demo/ProyectoEjemploApplication.java
+++ src/main/java/com/example/demo/ProyectoEjemploApplication.java
@@ -2,18 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class ProyectoEjemploApplication {
     public static void main(String[] args) {
             SpringApplication.run(ProyectoEjemploApplication.class, args);
     }
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "world")String name){
-        return String.format("Hello %s!", name);
+    public String hello(@RequestParam(value = "name", defaultValue = "world")String name, Model model){
+        // El model sirve para pasar información a la vista.
+        model.addAttribute("nombre", name);
+        
+        return "bienvenida";
     }
 }
